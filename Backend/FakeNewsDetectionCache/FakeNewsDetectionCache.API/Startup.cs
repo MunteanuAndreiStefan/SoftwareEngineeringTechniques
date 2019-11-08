3f8dbce8f6fe7780b54b9179e33120ceb8e32a5a
PantelemonVictorStefan <victorboss97@yahoo.com>
2019-11-08 02:00:41 +0200

    Added swagger
    Added swagger, removed some files

--- Backend/FakeNewsDetectionCache/FakeNewsDetectionCache.API/Startup.cs
+++ Backend/FakeNewsDetectionCache/FakeNewsDetectionCache.API/Startup.cs
@@ -13,7 +13,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace FakeNewsDetectionCache.API
 {
@@ -40,6 +40,14 @@ namespace FakeNewsDetectionCache.API
 
 
 
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +60,18 @@ namespace FakeNewsDetectionCache.API
 
             app.UseHttpsRedirection();
 
+
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                //c.RoutePrefix = string.Empty;
+            });
+
+
             app.UseRouting();
 
             app.UseAuthorization();
@@ -60,6 +80,9 @@ namespace FakeNewsDetectionCache.API
             {
                 endpoints.MapControllers();
             });
+
+            
+
         }
     }
 }
